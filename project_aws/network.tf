c78f359279528212f141b81caa437e01d7c83f0d
Yago Ésquines <yago.fut@gmail.com>
2021-01-23 18:48:09 -0300

    refactor: network variables and loop forms were added to organize and improve resource creation

--- project_aws/network.tf
+++ project_aws/network.tf
@@ -1,21 +1,22 @@
+// Provide a VPC with publics and privates subnets and a security group
 resource "aws_vpc" "vpc_web" {
-  cidr_block           = "192.168.0.0/16"
-  enable_dns_hostnames = true
-  tags                 = { Name = "vpc-web" }
+  cidr_block           = var.vpc_network
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
+  tags                 = { Name = var.vpc_name }
 }
 
 resource "aws_internet_gateway" "igw_web" {
   vpc_id = aws_vpc.vpc_web.id
-  tags   = { Name = "igw-web" }
+  tags   = { Name = var.igw_name }
 }
 
 resource "aws_route_table" "route_igw" {
   vpc_id = aws_vpc.vpc_web.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.route_default_cidr
     gateway_id = aws_internet_gateway.igw_web.id
   }
-  tags = { Name = "route-igw" }
+  tags = { Name = var.route_igw_name }
 }
 
 resource "aws_main_route_table_association" "association_igw" {
@@ -23,38 +24,22 @@ resource "aws_main_route_table_association" "association_igw" {
   route_table_id = aws_route_table.route_igw.id
 }
 
-resource "aws_subnet" "private_subnet1" {
+resource "aws_subnet" "private_subnet" {
+  for_each          = var.private_subnet
   vpc_id            = aws_vpc.vpc_web.id
-  cidr_block        = "192.168.1.0/24"
-  availability_zone = "us-east-1a"
-  tags              = { Name = "private-subnet-1" }
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  tags              = { Name = each.key }
 }
 
-resource "aws_subnet" "private_subnet2" {
-  vpc_id            = aws_vpc.vpc_web.id
-  cidr_block        = "192.168.2.0/24"
-  availability_zone = "us-east-1b"
-  tags              = { Name = "private-subnet-2" }
-}
-
-resource "aws_subnet" "public_subnet1" {
+resource "aws_subnet" "public_subnet" {
+  for_each                = var.public_subnet
   vpc_id                  = aws_vpc.vpc_web.id
   map_public_ip_on_launch = true
-  cidr_block              = "192.168.3.0/24"
-  availability_zone       = "us-east-1a"
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.availability_zone
   tags = {
-    Name    = "public-subnet-1"
-    Network = "public"
-  }
-}
-
-resource "aws_subnet" "public_subnet2" {
-  vpc_id                  = aws_vpc.vpc_web.id
-  map_public_ip_on_launch = true
-  cidr_block              = "192.168.4.0/24"
-  availability_zone       = "us-east-1b"
-  tags = {
-    Name    = "public-subnet-2"
+    Name    = each.key
     Network = "public"
   }
 }
@@ -65,32 +50,28 @@ resource "aws_eip" "eip_public" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.eip_public.id
-  subnet_id     = aws_subnet.public_subnet1.id
-  tags          = { Name = "nat-gw" }
+  subnet_id     = aws_subnet.public_subnet["public_subnet1"].id
+  tags          = { Name = var.natgw_name }
   depends_on    = [aws_internet_gateway.igw_web]
 }
 
 resource "aws_route_table" "route_natgw" {
   vpc_id = aws_vpc.vpc_web.id
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block     = var.route_default_cidr
     nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
-  tags = { Name = "route-natgw" }
+  tags = { Name = var.route_natgw_name }
 }
 
-resource "aws_route_table_association" "association_natgw1" {
-  subnet_id      = aws_subnet.private_subnet1.id
-  route_table_id = aws_route_table.route_natgw.id
-}
-
-resource "aws_route_table_association" "association_natgw2" {
-  subnet_id      = aws_subnet.private_subnet2.id
+resource "aws_route_table_association" "association_natgw" {
+  for_each       = var.private_subnet
+  subnet_id      = aws_subnet.private_subnet[each.key].id
   route_table_id = aws_route_table.route_natgw.id
 }
 
 resource "aws_security_group" "fw_web" {
-  name   = "fw-web"
+  name   = var.sg_name
   vpc_id = aws_vpc.vpc_web.id
 
   ingress {
@@ -106,6 +87,6 @@ resource "aws_security_group" "fw_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = "fw-web" }
-
+  tags = { Name = var.sg_name }
 }
+
