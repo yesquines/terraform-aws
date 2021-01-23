28d88cfa2d3c60c1de1c941edb428c6302fd6927
Yago Ésquines <yago.fut@gmail.com>
2021-01-23 18:52:07 -0300

    refactor: added ec2 variables
    style: data block was migraded to data.tf file
    
    fix: adjusted subnet_id call

--- project_aws/ec2.tf
+++ project_aws/ec2.tf
@@ -1,30 +1,22 @@
+//Launch an ec2 instance with a role inside the private subnet of VPC, and install apache through bootstrapping.
 resource "aws_iam_instance_profile" "s3_access_profile" {
-  name = "s3_access_profile-ec2"
+  name = var.instance_profile_name
   role = aws_iam_role.s3_access_role.name
 }
 
-data "aws_ami" "image_ec2" {
-  owners      = ["136693071363"] #Debian ID - https://wiki.debian.org/Cloud/AmazonEC2Image/Marketplace
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["debian-10-amd64-*"]
-  }
-}
-
 resource "time_sleep" "wait_time" {
-  depends_on      = [aws_route_table_association.association_natgw1, aws_route_table_association.association_natgw2]
+  depends_on      = [aws_route_table_association.association_natgw]
   create_duration = "30s"
 }
 
 resource "aws_instance" "instace_web" {
   ami                    = data.aws_ami.image_ec2.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_subnet1.id
+  instance_type          = var.ec2_type
+  subnet_id              = aws_subnet.private_subnet["private_subnet1"].id
   iam_instance_profile   = aws_iam_instance_profile.s3_access_profile.id
-  user_data              = file("files/apache.sh")
+  user_data              = file(var.userdata_file_path)
   vpc_security_group_ids = [aws_security_group.fw_web.id]
   depends_on             = [time_sleep.wait_time]
-  tags                   = { Name = "instance-web" }
+  tags                   = { Name = var.ec2_name }
 }
 
