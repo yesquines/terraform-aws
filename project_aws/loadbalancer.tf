9961d3aafdbdbd6008046df62dee8f10f55e50ec
Yago Ésquines <yago.fut@gmail.com>
2021-01-23 18:52:52 -0300

    refactor: added loadbalancer variables
    style: data block was migraded to data.tf file

--- project_aws/loadbalancer.tf
+++ project_aws/loadbalancer.tf
@@ -1,40 +1,33 @@
-data "aws_subnet_ids" "lb_subnets" {
-  vpc_id     = aws_vpc.vpc_web.id
-  depends_on = [aws_subnet.public_subnet1, aws_subnet.public_subnet2]
-  tags = {
-    Network = "public"
-  }
-}
-
+//Create a load balancer in public subnet and Add the ec2 instance, under the load balancer
 resource "aws_lb" "lb_web" {
-  name               = "lb-web"
-  load_balancer_type = "application"
+  name               = var.loadbalancer_name
+  load_balancer_type = var.loadbalancer_type
   security_groups    = [aws_security_group.fw_web.id]
   subnets            = data.aws_subnet_ids.lb_subnets.ids
   internal           = false
-
 }
 
 resource "aws_lb_target_group" "lb_tg" {
-  name     = "lb-tg-web"
-  port     = 80
-  protocol = "HTTP"
+  name     = var.loadbalancer_targetgroup_name
+  port     = var.loadbalancer_port
+  protocol = var.loadbalancer_protocol
   vpc_id   = aws_vpc.vpc_web.id
 }
 
 resource "aws_lb_target_group_attachment" "tg_attach" {
   target_group_arn = aws_lb_target_group.lb_tg.arn
   target_id        = aws_instance.instace_web.id
-  port             = 80
+  port             = var.loadbalancer_port
 }
 
 resource "aws_lb_listener" "lb_backend" {
   load_balancer_arn = aws_lb.lb_web.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = var.loadbalancer_port
+  protocol          = var.loadbalancer_protocol
 
   default_action {
-    type             = "forward"
+    type             = var.lb_action_type
     target_group_arn = aws_lb_target_group.lb_tg.arn
   }
 }
+
