e93c701f34872c8b5c9bdbb482d6f73a6d66c6c3
Yago Ésquines <yago.fut@gmail.com>
2021-01-23 18:44:35 -0300

    refactor: added roles variables

--- project_aws/role.tf
+++ project_aws/role.tf
@@ -1,15 +1,16 @@
 // Create a role with s3 access 
 resource "aws_iam_role" "s3_access_role" {
-  name               = "s3-access-role"
-  assume_role_policy = file("files/s3_role.json")
+  name               = var.s3_role_name
+  assume_role_policy = file(var.s3_role_file_path)
 }
 
 resource "aws_iam_policy" "s3_access_policy" {
-  name   = "s3-access-policy"
-  policy = file("files/s3_policy.json")
+  name   = var.s3_policy_name
+  policy = file(var.s3_policy_file_path)
 }
 
 resource "aws_iam_role_policy_attachment" "s2_access_attach_policy" {
   role       = aws_iam_role.s3_access_role.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
+
